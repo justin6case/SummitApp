29e3bea7eadb2938ecca53c3b027e1aafaa8d3b8
Matt Case <matt@mattcase.com>
2017-03-12 13:37:48 -0500

    Fixing indentation.

--- src/pages/play-media/play-media.ts
+++ src/pages/play-media/play-media.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, NavParams, AlertController } from 'ionic-angular';
 import { MediaPlugin } from 'ionic-native';
 import { VideoPlayer } from 'ionic-native';
+import {MusicControls} from 'ionic-native';
 
 @Component({
   templateUrl: 'play-media.html'
@@ -20,109 +21,108 @@ export class PlayMediaPage {
     console.log('ionViewDidLoad PlayMediaPage');
   }
 
+  loadAudio(audioFileUrl) {
+    try {
+      console.log("Loading audio file " + audioFileUrl);
+      this.file = new MediaPlugin(audioFileUrl, response => {this.fileStatus = response; console.log("Response is " + response);} );
+      this.url = audioFileUrl;
+    }
+    catch (e) {
+      this.showAlert('Error loading file');
+    }
+  }
 
-     loadAudio(audioFileUrl) {
-       try {
-         console.log("Loading audio file " + audioFileUrl);
-         this.file = new MediaPlugin(audioFileUrl, response => {this.fileStatus = response; console.log("Response is " + response);} );
-         this.url = audioFileUrl;
-       }
-       catch (e) {
-         this.showAlert('Error loading file');
-       }
-     }
+  playPauseHandler(article) {
+    // file is new, no previous file, load new file, play
+    // file is new, previous file was being played, stop old file, load new file, play
+    // file is not new, file is being played, stopped, or paused
 
-     playPauseHandler(article) {
-       // file is new, no previous file, load new file, play
-       // file is new, previous file was being played, stop old file, load new file, play
-       // file is not new, file is being played, stopped, or paused
-
-       // file is new, no previous file, load new file, play
-       if (this.url == null) {
-         console.log("New file");
-         try {
-           this.loadAudio(article.link);
-         }
-         catch (e) {
-            this.showAlert('Could not play 1.');
-            return;
-         }
-         if (this.url != null) {
-           this.playAudio();
-         }
-       }
-       // file is new, previous file was being played, stop old file, load new file, play
-       else if (this.url != article.link) {
-         console.log("Switching files")
-          this.stopAudio();
-          try {
-            this.loadAudio(article.link);
-          }
-          catch (e) {
-             this.showAlert('Could not play 2.');
-             return;
-          }
-          if (this.url != null) {
-            this.playAudio();
-          }
-       }
-       // file is not new, file is being played, stopped, or paused
-       else if (this.url == article.link) {
-         console.log("play/pause file");
-          // file is being played
-          if (this.fileStatus == MediaPlugin.MEDIA_RUNNING ||
-              this.fileStatus == MediaPlugin.MEDIA_STARTING) {
-            this.pauseAudio();
-            console.log("pausing" + this.url);
-          }
-          // file is paused or stopped
-          else {
-            this.playAudio();
-            console.log("playing" + this.url);
-          }
-       }
-     }
-
-     stopHandler() {
-       if (this.file != null) {
-         this.stopAudio();
-       }
-     }
-
-      playAudio() {
-        try {
-          this.file.play();
+    // file is new, no previous file, load new file, play
+    if (this.url == null) {
+      console.log("New file");
+      try {
+        this.loadAudio(article.link);
+      }
+      catch (e) {
+        this.showAlert('Could not play 1.');
+        return;
+      }
+      if (this.url != null) {
+        this.playAudio();
+      }
+    }
+    // file is new, previous file was being played, stop old file, load new file, play
+    else if (this.url != article.link) {
+      console.log("Switching files")
+      this.stopAudio();
+      try {
+        this.loadAudio(article.link);
+      }
+      catch (e) {
+        this.showAlert('Could not play 2.');
+        return;
+      }
+      if (this.url != null) {
+        this.playAudio();
+      }
+    }
+    // file is not new, file is being played, stopped, or paused
+    else if (this.url == article.link) {
+      console.log("play/pause file");
+      // file is being played
+      if (this.fileStatus == MediaPlugin.MEDIA_RUNNING ||
+        this.fileStatus == MediaPlugin.MEDIA_STARTING) {
+          this.pauseAudio();
+          console.log("pausing" + this.url);
         }
-        catch (e) {
-          this.showAlert('Could not play 3.');
+        // file is paused or stopped
+        else {
+          this.playAudio();
+          console.log("playing" + this.url);
         }
       }
+    }
 
-     pauseAudio() {
-       try {
-         this.file.pause();
-       }
-       catch (e) {
-         this.showAlert('Could not pause.');
-       }
-     }
-
-     stopAudio() {
-       try {
-         this.file.stop();
-       }
-       catch (e) {
-         this.showAlert('Could not stop.');
-       }
-     }
-
-     showAlert(message) {
-        let alert = this.alertCtrl.create({
-           title: 'Error',
-           subTitle: message,
-           buttons: ['OK']
-        });
-       alert.present();
+    stopHandler() {
+      if (this.file != null) {
+        this.stopAudio();
       }
+    }
 
-}
+    playAudio() {
+      try {
+        this.file.play();
+      }
+      catch (e) {
+        this.showAlert('Could not play 3.');
+      }
+    }
+
+    pauseAudio() {
+      try {
+        this.file.pause();
+      }
+      catch (e) {
+        this.showAlert('Could not pause.');
+      }
+    }
+
+    stopAudio() {
+      try {
+        this.file.stop();
+      }
+      catch (e) {
+        this.showAlert('Could not stop.');
+      }
+    }
+
+    showAlert(message) {
+      let alert = this.alertCtrl.create({
+        title: 'Error',
+        subTitle: message,
+        buttons: ['OK']
+      });
+      alert.present();
+    }
+
+  }
