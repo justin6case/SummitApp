bc73620c0fd254b1196907da979cd6ebdb4164b2
Matt Case <matt@mattcase.com>
2017-03-12 18:56:26 -0500

    Fixing issue with media file status for ios.

--- src/pages/play-media/play-media.ts
+++ src/pages/play-media/play-media.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, NavParams, AlertController } from 'ionic-angular';
 import { MediaPlugin } from 'ionic-native';
 import { VideoPlayer } from 'ionic-native';
-import {MusicControls} from 'ionic-native';
+import { Platform } from 'ionic-angular';
 
 @Component({
   templateUrl: 'play-media.html'
@@ -12,9 +12,51 @@ export class PlayMediaPage {
   url: string;
   file: any;
   fileStatus: number;
+  platform: any;
+  none: number;
+  starting: number;
+  running: number;
+  paused: number;
+  stopped: number;
 
-  constructor(public navCtrl: NavController, public alertCtrl: AlertController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController, public navParams: NavParams, public plt: Platform) {
     this.item = navParams.get('item');
+    this.fileStatus = 0;
+    if ( plt.is('ios')) {
+      this.platform = 'ios';
+      // ios statuses are different than android
+      this.none = MediaPlugin.MEDIA_NONE;
+      this.starting = MediaPlugin.MEDIA_STARTING;
+      this.running = MediaPlugin.MEDIA_PAUSED;
+      this.paused = MediaPlugin.MEDIA_RUNNING;
+      this.stopped = MediaPlugin.MEDIA_STOPPED;
+    }
+    else if ( plt.is('android')) {
+      this.platform = 'android';
+      this.none = MediaPlugin.MEDIA_NONE;
+      this.starting = MediaPlugin.MEDIA_STARTING;
+      this.running = MediaPlugin.MEDIA_RUNNING;
+      this.paused = MediaPlugin.MEDIA_PAUSED;
+      this.stopped = MediaPlugin.MEDIA_STOPPED;
+    }
+    else if ( plt.is('windows')) {
+      this.platform = 'windows';
+      this.none = MediaPlugin.MEDIA_NONE;
+      this.starting = MediaPlugin.MEDIA_STARTING;
+      this.running = MediaPlugin.MEDIA_RUNNING;
+      this.paused = MediaPlugin.MEDIA_PAUSED;
+      this.stopped = MediaPlugin.MEDIA_STOPPED;
+    }
+    else if ( plt.is('cordova')) {
+      this.platform = 'cordova';
+      this.none = MediaPlugin.MEDIA_NONE;
+      this.starting = MediaPlugin.MEDIA_STARTING;
+      this.running = MediaPlugin.MEDIA_RUNNING;
+      this.paused = MediaPlugin.MEDIA_PAUSED;
+      this.stopped = MediaPlugin.MEDIA_STOPPED;
+    }
+    console.log(this.platform);
+
   }
 
   ionViewDidLoad() {
@@ -24,6 +66,7 @@ export class PlayMediaPage {
   loadAudio(audioFileUrl) {
     try {
       console.log("Loading audio file " + audioFileUrl);
+      this.fileStatus = 1;
       this.file = new MediaPlugin(audioFileUrl, response => {this.fileStatus = response; console.log("Response is " + response);} );
       this.url = audioFileUrl;
     }
